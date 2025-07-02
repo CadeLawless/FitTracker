748a2f1f8574c4cbaef604774909a092409c9261
Cade Lawless <clawless05@gmail.com>
2025-07-02 14:38:42 -0500

    Updated MeasurementsTracker.tsx

--- src/components/MeasurementsTracker.tsx
+++ src/components/MeasurementsTracker.tsx
@@ -541,7 +541,7 @@ export default function MeasurementsTracker() {
     <>
       {/* Notifications */}
       {notifications.length > 0 && (
-        <div className="fixed top-4 right-4 z-50 space-y-2 w-[min(100%,500px)]">
+        <div className="fixed top-4 right-4 z-50 space-y-2 w-[min(100%,400px)]">
           {notifications.map((notification) => (
             <div
               key={notification.id}
