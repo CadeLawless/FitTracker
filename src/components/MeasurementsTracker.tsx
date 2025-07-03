451a03acf368804f89494a360fbfa76bd774efc7
Cade Lawless <clawless05@gmail.com>
2025-07-03 10:50:56 -0500

    Updated MeasurementsTracker.tsx

--- src/components/MeasurementsTracker.tsx
+++ src/components/MeasurementsTracker.tsx
@@ -764,7 +764,7 @@ export default function MeasurementsTracker() {
                     </p>
                     {change !== null && (
                       <p className="text-xs text-gray-500">
-                        {change > 0 ? '+' : ''}{change.toFixed(1)} {field.unit} all time
+                        All Time {change > 0 ? '+' : ''}{change.toFixed(1)} {field.unit}
                       </p>
                     )}
                   </div>
