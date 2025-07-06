14c365ce7c5e6e1fcc138249b64ad86ed7fe11a2
Cade Lawless <clawless05@gmail.com>
2025-07-06 10:14:50 -0500

    Updated WorkoutSessionDetails.tsx

--- src/components/WorkoutSessionDetails.tsx
+++ src/components/WorkoutSessionDetails.tsx
@@ -369,7 +369,7 @@ export default function WorkoutSessionDetails() {
             {totalDurationMinutes > 0 ? (
               <Timer className="h-6 w-6 lg:h-8 lg:w-8 text-orange-600 flex-shrink-0" />
             ) : (
-              <Clock className="h-6 w-6 lg:h-8 lg:w-8 text-orange-600 flex-shrink-0" />
+              <Scale className="h-6 w-6 lg:h-8 lg:w-8 text-orange-600 flex-shrink-0" />
             )}
             <div className="ml-3 lg:ml-4 min-w-0 flex-1">
               <p className="text-xs lg:text-sm font-medium text-gray-600">{totalDurationMinutes > 0 ? "Exercise Time" : "Avg Weight"}</p>
