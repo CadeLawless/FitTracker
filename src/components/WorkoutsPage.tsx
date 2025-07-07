4f97783d0afb3333aeecd16a9da9dfca5ab14e18
Cade Lawless <clawless05@gmail.com>
2025-07-07 12:32:13 -0500

    Updated WorkoutsPage.tsx

--- src/components/WorkoutsPage.tsx
+++ src/components/WorkoutsPage.tsx
@@ -248,7 +248,7 @@ export default function WorkoutsPage() {
           <div className="bg-white rounded-lg shadow-xl w-full max-w-md max-h-[90vh] overflow-y-auto">
             <div className="p-6">
               <div className="flex items-center justify-between">
-                <h3 className="text-lg font-medium mb-3 text-gray-900">
+                <h3 className="text-lg font-medium text-gray-900">
                   Start Workout
                 </h3>
                 <button
