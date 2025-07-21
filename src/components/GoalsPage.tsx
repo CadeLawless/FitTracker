65657bb447049e0d11124ed94f2426d9bc795976
Cade Lawless <clawless05@gmail.com>
2025-07-21 14:22:37 -0500

    Updated GoalsPage.tsx

--- src/components/GoalsPage.tsx
+++ src/components/GoalsPage.tsx
@@ -851,7 +851,7 @@ export default function GoalsPage() {
 
                   <label className={`relative flex cursor-pointer rounded-lg border p-4 focus:outline-none ${
                     formData.goal_category === 'measurement'
-                      ? 'border-blue-500 bg-blue-50'
+                      ? 'border-blue-500 dark:border-blue-200 bg-blue-50 dark:bg-blue-600/10'
                       : 'border-gray-300 bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-900/50'
                   }`}>
                     <input
