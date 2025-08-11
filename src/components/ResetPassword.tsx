544b6242a329f029de757ce54d91073067349d0e
CadeLawless <clawless05@gmail.com>
2025-08-11 13:42:57 -0500

    fixed success message

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -169,27 +169,25 @@ export default function ResetPassword() {
         }
 
         {success &&
-          <div className='text-center'>
-            <Link
-              to={'/'}
-              className="inline-block mt-4 py-2.5 lg:py-3 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors"
-            >
-              Go to Login
-            </Link>
-          </div>
+          <>
+            <div className="bg-green-50 dark:bg-green-500/10 dark:bg-green-900/50 border border-green-200 dark:border-green-400/40 dark:border-green-800 text-green-600 dark:text-green-400 px-4 py-3 rounded-lg text-sm">
+              {message}
+            </div>
+            <div className='text-center'>
+              <Link
+                to={'/'}
+                className="inline-block mt-4 py-2.5 lg:py-3 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors"
+              >
+                Go to Login
+              </Link>
+            </div>
+          </>
         }
 
         {!success && passwordReset && !resetExpired && !passwordReset.used &&
           <>
             {/* Form */}
             <form className="mt-6 lg:mt-8 space-y-4 lg:space-y-6" onSubmit={handleSubmit}>
-
-              {message &&
-                <div className="bg-green-50 dark:bg-green-500/10 dark:bg-green-900/50 border border-green-200 dark:border-green-400/40 dark:border-green-800 text-green-600 dark:text-green-400 px-4 py-3 rounded-lg text-sm">
-                  {message}
-                </div>
-              }
-
               {error && (
                 <div className="bg-red-50 dark:bg-red-500/10 dark:bg-red-900/50 border border-red-200 dark:border-red-400/40 dark:border-red-800 text-red-600 dark:text-red-400 px-4 py-3 rounded-lg text-sm">
                   {error}
