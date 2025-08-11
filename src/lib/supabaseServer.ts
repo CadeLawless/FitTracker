6fbb08652836338a91309a62a0150ff6cf08ef4a
CadeLawless <clawless05@gmail.com>
2025-08-11 08:57:00 -0500

    added dumbbell png to reset email

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -4,8 +4,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.SERVER_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.SERVER_SUPABASE_SERVICE_ROLE_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
